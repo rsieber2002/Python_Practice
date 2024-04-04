660b760c877b4155ba74b8678af6c0b882b47426
rsiebs <152230717+rsieber2002@users.noreply.github.com>
2024-04-04 17:00:58 +0000

    Creation of a basic class with some user inputs and an output

---
+++ Classes.py
@@ -0,0 +1,16 @@
+#This file is for the creation of basic classes and practice of classes in Python.
+
+#This is a basic class just to test some methodologies, refamiliarize myself with some basic python, and have some fun
+class Cadet:
+
+    #Like a construction, also creates attributes of the class
+    def __init__(self, name, grade, major): 
+        self.name = input("What is the Cadet's name?")
+        self.grade = input("What is the MS level of the Cadet?")
+        self.major = input("What is the major of the Cadet?")
+
+#Practice creation of an instance of the class
+person = Cadet(1, 2, 3) 
+
+#Output information about the created object
+print("The cadet's name is " + person.name + ", his or her MS level is " + person.grade + ", and his or her major is " + person.major)
