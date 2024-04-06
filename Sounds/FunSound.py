1ee6415b5e14cf2a89248726be71690cc16df2cf
rsiebs <152230717+rsieber2002@users.noreply.github.com>
2024-04-06 22:51:48 +0000

    Sound Changes

--- Sounds/FunSound.py
+++ Sounds/FunSound.py
@@ -1,5 +1,5 @@
-from playsound import playsound
+#import playsound
 
-playsound("/workspaces/Python_Practice/Sounds/ReverbFart.mp3")
+#playsound("/workspaces/Python_Practice/Sounds/ReverbFart.mp3")
 
 #Having some issues getting this to work
