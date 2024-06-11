4e2e538e61dce5c6950ff016a50e2c38f2390ede
rsiebs <152230717+rsieber2002@users.noreply.github.com>
2024-06-11 01:26:32 +0000

    Fixes 2

--- Random Summer Practice/computation.py
+++ Random Summer Practice/computation.py
@@ -1,8 +1,8 @@
 #This file is just to practice some basic computations utilizing prompts and variables
 
 #Get variables
-var1 = input("Please enter a number: ")
-var2 = input("Please enter a second number: ")
+var1 = float(input("Please enter a number: "))
+var2 = float(input("Please enter a second number: "))
 
 operation = input("Please enter one of the following: +, -, /, or *")
 
